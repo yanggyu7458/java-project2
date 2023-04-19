88a99978cb09ce962311ac7c7f5453fcc8ff46c9
yanggyu7458 <yang7458@naver.com>
2023-04-19 11:37:58 +0900

    feat : Bye기능 추가 - ByeClass, ByeClassTest 추가

---
+++ src/main/java/com/example/javaproject3/ByeClass.java
@@ -0,0 +1,7 @@
+package com.example.javaproject3;
+
+public class ByeClass {
+       public void print() {
+       System.out.println("Bye");
+   }
+}
---
+++ src/main/java/com/example/javaproject3/ByeClassTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject3;
+
+public class ByeClassTest {
+    public static void main(String[] args) {
+       ByeClass printBye = new ByeClass(); // 인스턴스화
+       printBye.print();
+   }
+}
