39bef29bed824a3ba632bddea34abdef2077102d
yanggyu7458 <yang7458@naver.com>
2023-04-19 14:56:08 +0900

    feat : BufferedReaderEx, BufferedReaderExTest 추가 -bufferedReader를 사용하여 한줄씩 읽어 두줄 읽기

---
+++ src/main/java/com/example/javaproject3/week1/BufferedReaderEx.java
@@ -0,0 +1,19 @@
+package com.example.javaproject3.week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReaderEx {
+    public void readALine() throws IOException {
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);
+
+       // String line = br.readLine();
+        System.out.println("line1 = " + br.readLine());
+        System.out.println("line2 = " + br.readLine());
+    }
+
+    public void readLine() {
+    }
+}
---
+++ src/main/java/com/example/javaproject3/week1/BufferedReaderExTest.java
@@ -0,0 +1,11 @@
+package com.example.javaproject3.week1;
+import java.io.IOException;
+public class BufferedReaderExTest {
+    public static void main(String[] args) throws IOException {
+        BufferedReaderEx bufferedReaderEx = new BufferedReaderEx();
+        bufferedReaderEx.readALine();
+
+
+    }
+
+}
