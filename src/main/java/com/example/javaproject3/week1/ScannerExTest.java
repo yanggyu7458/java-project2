086f215444c4ac886aa0f998ef9877cc26bca25f
yanggyu7458 <yang7458@naver.com>
2023-04-19 15:30:03 +0900

    feat : ScannerEx, ScannerExTest 추가 -Scanner를 사용하여 두 숫자를 입력하고 두 수를 더해 출력하기

---
+++ src/main/java/com/example/javaproject3/week1/ScannerExTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject3.week1;
+
+public class ScannerExTest {
+    public static void main(String[] args) {
+        ScannerEx se = new ScannerEx();
+        se.readTwoNumbersAndPlus();
+    }
+}
