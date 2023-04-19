258181ef482c8cb29d782056d4f9bb1b072e8894
yanggyu7458 <yang7458@naver.com>
2023-04-19 13:10:11 +0900

    feat : 마지막 크롤시간 작업1 -CrawlDate 도메인 추가

---
+++ src/main/java/com/example/javaproject3/CrawlDate.java
@@ -0,0 +1,9 @@
+package com.example.javaproject3;
+
+public class CrawlDate {
+    private LoacalDateTime lastCrawlDate;
+
+    public CrawlDate(LoacalDateTime lastCrawlDate) {
+        this.lastCrawlDate = lastCrawlDate;
+    }
+}
