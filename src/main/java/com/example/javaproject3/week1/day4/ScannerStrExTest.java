8044b4af6c1e5b2d9f72a22ef65b2264b46a1ee9
yanggyu7458 <yang7458@naver.com>
2023-04-20 09:41:47 +0900

    feat : ScannerStrEx -sc.next() 사용해보기

---
+++ src/main/java/com/example/javaproject3/week1/day4/ScannerStrExTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject3.week1.day4;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx scannerStrEx = new ScannerStrEx();
+        scannerStrEx.inputStringsAndPlus();
+    }
+}
