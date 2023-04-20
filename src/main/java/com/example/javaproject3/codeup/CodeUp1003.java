957e17b48aa662a2afb59ab303c77b7295781e8a
yanggyu7458 <yang7458@naver.com>
2023-04-20 09:17:59 +0900

    feat : CodeUp1-5

---
+++ src/main/java/com/example/javaproject3/codeup/CodeUp1003.java
@@ -0,0 +1,9 @@
+package com.example.javaproject3.codeup;
+
+public class CodeUp1003 {
+    public static void main(String[] args) {
+            System.out.println("Hello");
+            System.out.println("World");
+    }
+
+}
