d6a65e2ac0f7430e25e234107584295a5b45f22e
yanggyu7458 <yang7458@naver.com>
2023-04-26 14:37:28 +0900

    feat : CodeUp1042~1045 -기초 산술연산

---
+++ src/main/java/com/example/javaproject3/codeup/CodeUp1044.java
@@ -0,0 +1,11 @@
+package com.example.javaproject3.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1044 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        System.out.println(a + 1);
+    }
+}
