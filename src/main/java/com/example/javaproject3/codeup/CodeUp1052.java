3f0b490e6a0efc9bd91e2e77dac86ad2deb228f6
yanggyu7458 <yang7458@naver.com>
2023-04-27 11:21:11 +0900

    feat : CodeUp1046-1052

---
+++ src/main/java/com/example/javaproject3/codeup/CodeUp1052.java
@@ -0,0 +1,13 @@
+package com.example.javaproject3.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1052 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int iVal1 = sc.nextInt();
+        int iVal2 = sc.nextInt();
+        System.out.println(iVal1 != iVal2 ? 1 : 0);
+    }
+}
